a6870c42e0a9cc6be65c2a1cf16d7bd602cdd536
Joana Palero <joana.palero24@gmail.com>
2018-05-31 23:25:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,6 @@ if array.length == 1
     array.join
     elsif array.length == 2
       array.join(" and ")
-      else array.length >= 3
-        "#{array[0...-1].join(', ')}, and #{array.last}"
+      
   end
 end
